1e25350d746762668a7719f43cdb210568adeab6
ray-synglyphx <ray@synglyphx.com>
2014-12-19 10:25:24 -0500

    Minor fix

--- SGXDataTransform/datamappingfunctionstrategy.cpp
+++ SGXDataTransform/datamappingfunctionstrategy.cpp
@@ -1,11 +1,14 @@
 #include "datamappingfunctionstrategy.h"
 
+namespace SynGlyphX {
 
-DataMappingFunctionStrategy::DataMappingFunctionStrategy()
-{
-}
+	DataMappingFunctionStrategy::DataMappingFunctionStrategy()
+	{
+	}
 
 
-DataMappingFunctionStrategy::~DataMappingFunctionStrategy()
-{
-}
+	DataMappingFunctionStrategy::~DataMappingFunctionStrategy()
+	{
+	}
+
+} //namespace SynGlyphX
