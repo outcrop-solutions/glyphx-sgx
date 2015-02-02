177cabf485e07dd1b0e1c648f909e8eef99f3099
ray-synglyphx <ray@synglyphx.com>
2015-02-02 01:05:52 -0500

    Updated version number to 0.6.7

--- GlyphDesigner/main.cpp
+++ GlyphDesigner/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[])
 {
-    SynGlyphX::Application::Setup("Glyph Designer", "0.6.6");
+    SynGlyphX::Application::Setup("Glyph Designer", "0.6.7");
     SynGlyphX::Application a(argc, argv);
 
     SynGlyphX::Application::SetupIcons(QIcon(":SGXGUI/Resources/synglyphx_x.ico"));
