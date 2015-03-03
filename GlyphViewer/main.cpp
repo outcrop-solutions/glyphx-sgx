dcc52f020e07701e481346c63177e435ea8b5694
ray-synglyphx <Ray@synglyphx.com>
2015-03-03 12:28:21 -0500

    Updated version numbers

--- GlyphViewer/main.cpp
+++ GlyphViewer/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-	SynGlyphX::Application::Setup("Glyph Viewer", "0.6.9");
+	SynGlyphX::Application::Setup("Glyph Viewer", "0.6.10");
 	SynGlyphX::Application a(argc, argv);
 
 	SynGlyphX::Application::SetupIcons(QIcon(":SGXGUI/Resources/synglyphx_x.ico"));
