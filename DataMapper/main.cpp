b9b2ed0477314dc4cef9eb8328ef93b27d25a26e
ray-synglyphx <Ray@synglyphx.com>
2015-03-20 18:14:21 -0400

    Fix for missing DLLs in ANTz export from Data Mapper

--- DataMapper/main.cpp
+++ DataMapper/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-    SynGlyphX::Application::Setup("Data Mapper", "0.6.12");
+    SynGlyphX::Application::Setup("Data Mapper", "0.6.13");
     SynGlyphX::Application a(argc, argv);
 
     SynGlyphX::Application::SetupIcons(QIcon(":SGXGUI/Resources/synglyphx_x.ico"));
