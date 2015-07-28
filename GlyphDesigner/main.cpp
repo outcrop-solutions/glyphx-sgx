9e4b3d65a3eaeb6edd81267f66245099f99fa8aa
ray-synglyphx <Ray@synglyphx.com>
2015-07-28 13:16:32 -0400

    Update version number to 0.7.10

--- GlyphDesigner/main.cpp
+++ GlyphDesigner/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Designer", "0.7.09");
+    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Designer", "0.7.10");
 	SynGlyphX::GlyphBuilderApplication a(argc, argv);
 
 	SynGlyphX::GlyphBuilderApplication::SetupIcons(QIcon(":SGXGUI/Resources/synglyphx_x.ico"));
