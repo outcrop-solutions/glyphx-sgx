7ab9f35308f41823263987b68a3371888fe75dba
ray-synglyphx <Ray@synglyphx.com>
2015-08-28 18:18:20 -0400

    Initial add of TreeEditView

---
+++ SGXGlyphGUI/treeeditview.cpp
@@ -0,0 +1,16 @@
+#include "treeeditview.h"
+
+namespace SynGlyphX {
+
+	TreeEditView::TreeEditView(QWidget *parent)
+		: TreeView(parent)
+	{
+
+	}
+
+	TreeEditView::~TreeEditView()
+	{
+
+	}
+
+} //namespace SynGlyphX
