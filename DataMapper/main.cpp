cad5bc0fec047f01de6c6ceaad82746361585d3c
ray-synglyphx <Ray@synglyphx.com>
2015-09-14 17:49:54 -0400

    Update to version 0.7.14

--- DataMapper/main.cpp
+++ DataMapper/main.cpp
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[])
 {
-    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Data Mapper", "0.7.13");
+    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Data Mapper", "0.7.14");
 	SynGlyphX::GlyphBuilderApplication a(argc, argv);
 
 	const QString dumpPath = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Minidumps";
