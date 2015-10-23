7f1f1ec90651d492cd0f91de999ee62b40a94324
ray-synglyphx <Ray@synglyphx.com>
2015-10-23 18:41:06 -0400

    Added single class for all non-mappable geometry properties

---
+++ SGXFoundation/nonmappablegeometryproperties.h
@@ -0,0 +1,50 @@
+///
+/// SynGlyphX Holdings Incorporated ("COMPANY") CONFIDENTIAL
+/// Copyright (c) 2013-2015 SynGlyphX Holdings Incorporated, All Rights Reserved.
+///
+/// NOTICE:  All information contained herein is, and remains the property of COMPANY. The intellectual and technical concepts contained
+/// herein are proprietary to COMPANY and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law.
+/// Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
+/// from COMPANY.  Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees, managers or contractors who have executed 
+/// Confidentiality and Non-disclosure agreements explicitly covering such access.
+///
+/// The copyright notice above does not evidence any actual or intended publication or disclosure  of  this source code, which includes  
+/// information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, 
+/// OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE 
+/// LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS  
+/// TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.                
+///
+
+#ifndef SYNGLYPHX_NONMAPPABLEGEOMETRYPROPERTIES_H
+#define SYNGLYPHX_NONMAPPABLEGEOMETRYPROPERTIES_H
+
+#include "sgxfoundation.h"
+#include "glyphgeometryinfo.h"
+
+namespace SynGlyphX {
+
+	class SGXFOUNDATION_API NonMappableGeometryProperties
+	{
+	public:
+		NonMappableGeometryProperties(GlyphGeometryInfo::Surface surface = GlyphGeometryInfo::Surface::Solid, double torusRatio = 0.1);
+		NonMappableGeometryProperties(const NonMappableGeometryProperties& properties);
+		virtual ~NonMappableGeometryProperties();
+
+		NonMappableGeometryProperties& operator=(const NonMappableGeometryProperties& properties);
+		bool operator==(const NonMappableGeometryProperties& properties) const;
+		bool operator!=(const NonMappableGeometryProperties& properties) const;
+
+		GlyphGeometryInfo::Surface GetSurface() const;
+		void SetSurface(GlyphGeometryInfo::Surface surface);
+
+		double GetTorusRatio() const;
+		void SetTorusRatio(double torusRatio);
+
+	private:
+		GlyphGeometryInfo::Surface m_surface;
+		double m_torusRatio;
+	};
+
+} //namespace SynGlyphX
+
+#endif //SYNGLYPHX_NONMAPPABLEGEOMETRYPROPERTIES_H
