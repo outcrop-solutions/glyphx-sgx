f0d0ae3df83cf488251eb4f33e4c3e65754c50d0
ray-synglyphx <Ray@synglyphx.com>
2016-01-08 18:46:38 -0500

    Updated installer to include data engine

--- GlyphDesigner/main.cpp
+++ GlyphDesigner/main.cpp
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Designer", "0.7.21");
+    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Designer", "0.7.22");
 	SynGlyphX::GlyphBuilderApplication a(argc, argv);
 
 	const QString dumpPath = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Minidumps";
