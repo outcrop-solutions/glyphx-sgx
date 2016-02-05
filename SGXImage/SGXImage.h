12ebec5dc2d99e16d3329093c6e55fdd8e3f72e7
ray-synglyphx <Ray@synglyphx.com>
2016-02-05 10:31:14 -0500

    Initial add of CMake project files

--- SGXImage/SGXImage.h
+++ SGXImage/SGXImage.h
@@ -4,8 +4,14 @@
 // that uses this DLL. This way any other project whose source files include this file see 
 // SGXIMAGE_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
-#ifdef SGXIMAGE_EXPORTS
+#ifdef WIN32
+
+#ifdef SGXImage_EXPORTS
 #define SGXIMAGE_API __declspec(dllexport)
 #else
 #define SGXIMAGE_API __declspec(dllimport)
+#endif
+
+#else
+#define SGXIMAGE_API
 #endif
