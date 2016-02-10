0dfd433633bf0dcba8e0bca0e54ccdb635323185
ray-synglyphx <Ray@synglyphx.com>
2016-02-10 14:19:19 -0500

    CMake update

--- DataEngine/DataEngine_Exports.h
+++ DataEngine/DataEngine_Exports.h
@@ -1,5 +1,11 @@
-#ifdef DATAENGINE_EXPORTS
+
+#ifdef WIN32
+#ifdef DataEngine_EXPORTS
 #define DATAENGINE __declspec(dllexport)
 #else
 #define DATAENGINE __declspec(dllimport)
+#endif
+
+#else
+#define DATAENGINE 
 #endif
