7fc5bc477cec7e65605a85235501a85dc8d41736
ray-synglyphx <Ray@synglyphx.com>
2016-02-29 20:13:59 -0500

    Update to version 0.7.24

--- GlyphDesigner/main.cpp
+++ GlyphDesigner/main.cpp
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Designer", "0.7.23");
+    SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Designer", "0.7.24");
 	SynGlyphX::GlyphBuilderApplication a(argc, argv);
 
 	const QString dumpPath = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Minidumps";
