6fdedbd0991c38d743b55a007c7fcaf2d71e3433
orin <otresnjak@gmail.com>
2016-06-01 14:57:10 -0400

    Replace hardcoded path with QTDIR

--- scripts/setupWindowsOutputBin.bat
+++ scripts/setupWindowsOutputBin.bat
@@ -60,9 +60,9 @@ FOR /F "tokens=*" %%p IN ('dir /b /a:d ..\bin\*') DO (
 		copy /B /Y "..\..\DataEngine\Java DataEngine\ConvertHash.dll" %basedir%\%%p\%%c\ConvertHash.dll
 
 		rem TODO: Needed files only.
-		robocopy /z /e "C:\Qt\Qt5.6.0\5.6\msvc2013\bin\*.dll" %basedir%\%%p\%%c
-		robocopy /z /e "C:\Qt\Qt5.6.0\5.6\msvc2013\bin\*.dll" %basedir%\%%p\%%c
+		robocopy /z /e "%QTDIR%\bin\*.dll" %basedir%\%%p\%%c
+		robocopy /z /e "%QTDIR%\bin\*.dll" %basedir%\%%p\%%c
 		mkdir %basedir%\%%p\%%c\qt_plugins
-		robocopy /z /e "C:\Qt\Qt5.6.0\5.6\msvc2013\plugins" %basedir%\%%p\%%c\qt_plugins
+		robocopy /z /e "%QTDIR%\plugins" %basedir%\%%p\%%c\qt_plugins
 	)
 )
