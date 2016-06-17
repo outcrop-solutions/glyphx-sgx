8d62c1785e925d2d0c59dbce526f42995804780e
ray-synglyphx <Ray@synglyphx.com>
2016-06-17 17:58:29 -0400

    Update version number for new release 0.7.46

--- GlyphViewer/main.cpp
+++ GlyphViewer/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 	QSurfaceFormat::setDefaultFormat( fmt );
 #endif
     
-	SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Viewer", "0.7.45");
+	SynGlyphX::GlyphBuilderApplication::Setup("Glyph Builder - Glyph Viewer", "0.7.46");
 	SynGlyphX::GlyphBuilderApplication a(argc, argv);
 	if (SynGlyphX::GlyphBuilderApplication::IsGlyphEd()) {
 
