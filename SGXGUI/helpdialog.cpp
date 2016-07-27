1df078e01e4ffaaa45f364d3593b8519c8c84626
Bryan-SynGlyphX <bryan@synglyphx.com>
2016-07-27 17:34:48 -0400

    Added basic help menu dialog

---
+++ SGXGUI/helpdialog.cpp
@@ -0,0 +1,44 @@
+#include "helpdialog.h"
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QDialogButtonBox>
+#include "application.h"
+#include <string>
+#include <QtWidgets/QMessageBox>
+#include "groupboxsinglewidget.h"
+#include <QtWidgets/QFileDialog>
+#include <QtCore/QSettings>
+#include <QtCore/QStandardPaths>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTextEdit>
+#include <QtGui/QTextDocument>
+#include <QtCore/QTextStream>
+#include "filesystem.h"
+#include <QtWidgets/QLabel>
+#include <QtWebEngineWidgets/QWebEngineView>
+
+namespace SynGlyphX {
+
+	HelpDialog::HelpDialog(QWidget *parent)
+		: QDialog(parent)
+	{
+		setWindowTitle(tr("Help"));
+
+		QVBoxLayout* layout = new QVBoxLayout(this);
+
+		QWebEngineView *view = new QWebEngineView(this);
+		view->setMinimumSize(920,870);
+		QString path = SynGlyphX::Application::GetCommonDataLocation() + QDir::separator() + "Help" + QDir::separator();
+		path.replace("\\", "/");
+		view->load(QUrl((path + "index.htm")));
+
+		layout->addWidget(view);
+
+		setLayout(layout);
+	}
+
+	HelpDialog::~HelpDialog()
+	{
+
+	}
+}
