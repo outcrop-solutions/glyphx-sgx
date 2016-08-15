23671047532366fab34d2e818e8cb6c2d76a11cc
Bryan-SynGlyphX <bryan@synglyphx.com>
2016-08-15 13:56:31 -0400

    Bug fix for DL Map and SQLite not working together

--- JavaDataEngine/GlyphEngine.java
+++ JavaDataEngine/GlyphEngine.java
@@ -146,8 +146,8 @@ public class GlyphEngine {
 	 	//String outDir = "C:/Users/Bryan/Desktop/TELCO Small Subset/Viz";
 	 	//String sdtPath = "C:/Users/Bryan/Desktop/GlobalAdmissions20132016/GlobalAdmissions20132016/View 1 Datamap Recommendations.sdt";
 	 	//String outDir = "C:/Users/Bryan/Desktop/GlobalAdmissions20132016/GlobalAdmissions20132016/Viz";
-	 	String sdtPath = "D:/Downloads/frontend_example.sdt";
-	 	String outDir = "D:/Downloads";
+	 	String sdtPath = "C:\\Users\\Bryan\\Desktop\\Test Files\\working_sqlite\\animatedrotate_planets_noscale.sdt";
+	 	String outDir = "C:\\Users\\Bryan\\Desktop\\Test Files\\working_sqlite";
 
 	 	String expDir = "DataMapper";
 	 	GlyphEngine start = new GlyphEngine();
@@ -163,7 +163,8 @@ public class GlyphEngine {
 	 	s[0] = 2048.0; s[1] = 1024.0;
 	 	//s[0] = 471.0; s[1] = 634.0;
 	 	int err = start.initiate(sdtPath, outDir, expDir);
-
+	 	double[] nwse = start.getNWandSE();
+/*
 	 	String id = "cdd96014-8e17-4d2a-927a-4a1f8ac4662c";
 	 	String table = "OnlyTable";
 	 	String field = "Name";
@@ -171,7 +172,7 @@ public class GlyphEngine {
 	 	for(int i = 0; i < distinct.length; i++)
 	 	{
 	 		System.out.println((i+1) +": "+ distinct[i]);
-	 	}
+	 	}*/
 /*
 	 	String query = "select * from FirstPortion where `OrbitalEcc.` between 0 and 0.2;";
 	 	start.setQueryForDatasource(id, table, query);
@@ -194,14 +195,14 @@ public class GlyphEngine {
 		}
 		String[] images = start.getBaseImages();
 		System.out.println(images.length);
-
-		if(err == 1){
+*/
+		if(err > 0){
 			String [] errors = start.getErrors();
-			//System.out.println("Error List:\n");
+			System.out.println("Error List:\n");
 			for(int i = 0; i < errors.length; i++){
-				//System.out.println(errors[i]);
+				System.out.println(errors[i]);
 			}
-		}*/
+		}
 		
 	} 
 
