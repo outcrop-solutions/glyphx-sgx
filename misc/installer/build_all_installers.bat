46be175b384ff208ce5435842e8febc2c42a3490
orin <otresnjak@gmail.com>
2016-12-15 12:07:07 -0500

    improve installer build script prompt

--- misc/installer/build_all_installers.bat
+++ misc/installer/build_all_installers.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Make sure you've set up your release directory and build all four executables before proceeding.
-echo Also make sure you've updated the version number in version.wxi.
+echo Also make sure you've updated the version numbers in version.wxi and in version.h.
 pause
 call build_glyphed_installer.bat
 call build_glyphviewer_installer.bat
