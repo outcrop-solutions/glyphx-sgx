5aba68f7da67ebb050484a941f3954c1dcf09231
orin <otresnjak@gmail.com>
2017-01-25 17:42:59 -0500

    64 bit installer fixes

--- misc/installer/build_glyphed_installer.bat
+++ misc/installer/build_glyphed_installer.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Building glyphed installer...
-candle glyphed.wxs shared_files.wxs
+candle -arch x64 glyphed.wxs shared_files.wxs
 light -ext WixUIExtension -cultures:en-us glyphed.wixobj shared_files.wixobj -o output/glyphed.msi -b ..\..\bin\Win64\Release\
--- misc/installer/build_glyphit_installer.bat
+++ misc/installer/build_glyphit_installer.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Building glyphit installer...
-candle glyphit.wxs shared_files.wxs
+candle -arch x64 glyphit.wxs shared_files.wxs
 light -ext WixUIExtension -cultures:en-us glyphit.wixobj shared_files.wixobj -o output/glyphit.msi -b ..\..\bin\Win64\Release\
