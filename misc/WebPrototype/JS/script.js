84d1f062390d7d44d16ca37a4c2b3f5ee66facaf
Aditya <aditya@synglyphx.com>
2017-06-08 16:38:15 -0400

    Changes to web prototype structure
    Signed-off-by: Aditya <aditya@synglyphx.com>

--- misc/WebPrototype/JS/script.js
+++ misc/WebPrototype/JS/script.js
@@ -1,6 +1,7 @@
 
  $(document).ready(function() {
-    
+      
+   init();
     // Prepares the select boxes (materialize)
     $('select').material_select();
 
@@ -170,9 +171,7 @@ function toggleNav() {
       tag: 'Google',
     }],
   });*/
-    $('.material-icons .close').on('chip.delete', function(e, chip){
-    alert("HEy!");
-  });
+  
 }
 
 //Toggles view of the top section of the filter side bar
