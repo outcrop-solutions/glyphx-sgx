6c72016d11a1a1bae34de482a321e983630bea78
Aditya <aditya@synglyphx.com>
2017-06-09 15:03:18 -0400

    Web-Prototype TopView chips change

--- misc/WebPrototype/JS/script2.js
+++ misc/WebPrototype/JS/script2.js
@@ -1,6 +1,36 @@
 function init() {
     console.log("called init");
-    $('.material-icons .close').on('chip.delete', function(e, chip){
-    alert("HEy!");
-  });
+    $('.tap-target').tapTarget('open');
+
+
+    $('i.close').on('click', function(e){
+      onCancelFilter(e);
+    });
+
+    
+    $('#clearAllFilters').on('click', function(e){
+      onClearAllFilters(e);
+    });
+
+    $('#toggleFilters').on('click', function(e){
+      onToggleFilters(e);
+    });
+}
+
+function onCancelFilter(e){
+  alert("onCancelFilter!");
+}
+
+function onClearAllFilters(e){
+  alert("onClearAllFilters!");
+}
+
+
+function onToggleFilters(e){
+  //alert("onToggleFilters!");
+  if($("#toggleFilters").html().split("<")[0] == "Hide")
+  $("#toggleFilters").html("Show" + "<" + $("#toggleFilters").html().split("<")[1]);
+  else
+  $("#toggleFilters").html("Hide"  + "<" + $("#toggleFilters").html().split("<")[1]);
+  
 }
