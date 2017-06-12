5c675b5a53be4251903af5e5ec565bccd08f4d26
Marwane Boudriga <mboudrig@gmu.edu>
2017-06-12 14:49:05 -0400

    filterNav optimized for react

--- misc/WebPrototype/React/src/index.js
+++ misc/WebPrototype/React/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TopNav from './topNav';
-import FilterBar from './filterBar';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
