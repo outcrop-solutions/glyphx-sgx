7c5585e3ca844016f35eb736b98908dd0fdd3c5e
Aditya <aditya@synglyphx.com>
2017-06-13 14:15:15 -0400

    Layout Changes(introducing react-flex-materialize)

--- misc/WebPrototype/React/src/filterNav.js
+++ misc/WebPrototype/React/src/filterNav.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import 'materialize-css/bin/materialize.js';
-import 'materialize-css/dist/css/materialize.css';
-import {Navbar, NavItem} from 'react-materialize';
 import 'font-awesome/css/font-awesome.css';
 import './filterNav.css';
 import SelectionField from './SelectionField.js';
@@ -10,6 +7,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import Range from './range.js';
+import {Flex} from 'react-flex-material';
 
 class FilterNav extends Component {
 	
@@ -53,49 +52,60 @@ class FilterNav extends Component {
     return (
       <div className="TopNav" style={{height: '100%'}}>
           {/* TOP SECTION */}
-		  <div id="TopView" className="container" style={{width:'93%',height: '30%'}}>
-		  
-		  {/* Row 1 */}
-		  <div className="row" style={{'marginBottom': '0px',height:'15%'}}>
-		  <div className="input-field col s9 filter-wrap filter-font" style={{marginTop: '0px'}}>
-			<SelectionField />
-		  </div>
-			 <div className="input-field col s3 filter-wrap filter-font marginTop">
-				<div>
-					<RaisedButton
-					  onClick={this.handleTouchTap1}
-					  label="Menu"
-					/>
-					<Popover
-					  open={this.state.menu.open}
-					  anchorEl={this.state.menu.anchorEl}
-					  onRequestClose={this.handleRequestClose}
-					>
-					  <Menu>
-						<MenuItem primaryText="Refresh" />
-						<MenuItem primaryText="Help &amp; feedback" />
-						<MenuItem primaryText="Settings" />
-						<MenuItem primaryText="Sign out" />
-					  </Menu>
-					</Popover>
-				</div>
-			 </div>
-		  </div>
-		  
-		  {/* Row 2 */}
-		  <div className="row" style={{'marginBottom': '0px',height:'50%'}}>
-		  </div>
-		  
-		  {/* Row 3 */}
-		  <div className="row" style={{'marginBottom': '0px',height:'10%'}}>
-		  </div>
-		  
-		  {/* Row 4 */}
-		  <div className="row" style={{'marginBottom': '0px',height:'25%'}}>
-		  </div>
-		  
-		  </div>
-          {/* BOTTOM SECTION */}
+			<Flex layout="column" style={{height: '100%'}}>
+				<Flex flex="30" id="TopView">
+							  
+							  {/* Row 1 */}
+							  <Flex layout="row" align="space-between center">
+							  
+									<Flex flex="80">
+									  <div className="input-field col s9 filter-wrap filter-font" style={{marginTop: '0px'}}>
+										<SelectionField />
+									  </div>
+									</Flex>
+									<Flex divider />
+										<Flex flex="20">
+										 <div className="input-field col s3 filter-wrap filter-font marginTop">
+											<div>
+												<RaisedButton
+												  onClick={this.handleTouchTap1}
+												  label="Menu"
+												/>
+												<Popover
+												  open={this.state.menu.open}
+												  anchorEl={this.state.menu.anchorEl}
+												  onRequestClose={this.handleRequestClose}
+												>
+												  <Menu>
+													<MenuItem primaryText="Refresh" />
+													<MenuItem primaryText="Help &amp; feedback" />
+													<MenuItem primaryText="Settings" />
+													<MenuItem primaryText="Sign out" />
+												  </Menu>
+												</Popover>
+											</div>
+										 </div>
+										 </Flex>
+								</Flex>
+								
+							  
+							  {/* Row 2 */}
+							  <Flex layout="row" style={{'marginBottom': '0px',height:'50%'}}>
+							  </Flex>
+							  
+							  {/* Row 3 */}
+							  <Flex layout="row" style={{'marginBottom': '0px',height:'10%'}}>
+							  </Flex>
+							  
+							  {/* Row 4 */}
+							  <Flex layout="row" style={{'marginBottom': '0px',height:'25%'}}>
+							  </Flex>
+				</Flex>				  
+							  {/* BOTTOM SECTION */}
+				<Flex flex="75">
+					<Range ></Range >
+				</Flex>
+			</Flex>
       </div>
     );
   }
