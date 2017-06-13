c97536347dd143ce6b2f7dba794cf7c5cb18dc6e
Aditya <aditya@synglyphx.com>
2017-06-13 16:01:24 -0400

    Top View Update

--- misc/WebPrototype/React/src/SelectionField.js
+++ misc/WebPrototype/React/src/SelectionField.js
@@ -6,18 +6,26 @@ injectTapEventPlugin();
 
 export default class SelectionField extends Component {
   
-  dataSource2 = ['View 1', 'View 2', 'View 3'];
+  dataSource2 = ['View 1', 'View 2', 'View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3','View 3',];
   
   onSelect(value,position) {
 	console.log("Selected " + value + " " + position);
+  }
+  
+  onclose(a,b,c){
+	 // debugger;
   }
   
   render() {
     return (
       <div>
         <AutoComplete
-	  style={{fontSize: '13px',width:'100%',height: '49px'}}
+	  style={{fontSize: '13px',width:'100%'}}
+	  listStyle={{fontSize: '1px'}}
+	  menuStyle={{fontSize: '1px'}}
 	  fullWidth={true}
+  maxSearchResults={5}
+	  onClose={this.onclose}
 	  textFieldStyle = {{fontSize: '13px'}}
       hintText={"Select to open a view"}
       filter={AutoComplete.caseInsensitiveFilter}
--- misc/WebPrototype/React/src/filterNav.js
+++ misc/WebPrototype/React/src/filterNav.js
@@ -6,9 +6,16 @@ import SelectionField from './SelectionField.js';
 import RaisedButton from 'material-ui/RaisedButton';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
+import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import Range from './range.js';
 import {Flex} from 'react-flex-material';
+import Divider from 'material-ui/Divider';
+
+const items = [];
+for (let i = 0; i < 50; i++ ) {
+  items.push(<MenuItem value={i} key={i} primaryText={'View '+i+''} />);
+}
 
 class FilterNav extends Component {
 	
@@ -18,20 +25,17 @@ class FilterNav extends Component {
     this.state  = {
       menu:{
 	  open: false,
-	  anchorOrigin: {
-        horizontal: 'left',
-        vertical: 'bottom',
-      },
-      targetOrigin: {
-        horizontal: 'left',
-        vertical: 'top',
-      },
+	  },
+	  viewSelect:{
+		  value: 10,
 	  }
     };
   }
   
-  handleTouchTap1 = (event) => {
-    // This prevents ghost click.
+  /**
+	* This method is called when the menu button is clicked. The menu button is in the top right corner of the filerbar.
+	**/
+  menuDropdownClick = (event) => {
     event.preventDefault();
 
     this.setState(
@@ -40,12 +44,27 @@ class FilterNav extends Component {
 		anchorEl: event.currentTarget,}
     });
   };
+  
+  /**
+	* This method is called when there is loss of focus when the menu is open.
+	**/
   handleRequestClose = () => {
     this.setState(
 	{
       menu: {open: false}
     });
   };
+  
+  /**
+	* This method is called when an item is selected in the "select view" dropdown that is present in the top left.
+	**/
+   onSelectViewChange = (event, index, value) => {
+    this.setState({viewSelect:{
+		  value: value,
+	  }});
+	console.log(value);
+  };
+
   
 	
   render() {
@@ -56,49 +75,58 @@ class FilterNav extends Component {
 				<Flex flex="30" id="TopView">
 							  
 							  {/* Row 1 */}
-							  <Flex layout="row" align="space-between center">
+								<Flex layout="row" align="space-between center"    style={{height: '20%'}}>
 							  
 									<Flex flex="80">
-									  <div className="input-field col s9 filter-wrap filter-font" style={{marginTop: '0px'}}>
-										<SelectionField />
-									  </div>
-									</Flex>
+									  <SelectField
+										value={this.state.viewSelect.value}
+										onChange={this.onSelectViewChange}
+										style={{width:"100%",fontSize:'13px'}}
+									  >
+										{items}
+									  </SelectField>
+								</Flex>
+									
+									
 									<Flex divider />
 										<Flex flex="20">
-										 <div className="input-field col s3 filter-wrap filter-font marginTop">
-											<div>
+										 	<div>
 												<RaisedButton
-												  onClick={this.handleTouchTap1}
+												  onClick={this.menuDropdownClick}
 												  label="Menu"
+												  buttonStyle={{color: 'rgb(0, 188, 212)'}}
+												  labelStyle={{fontSize: '13px'}}
+												  primary={true}
 												/>
 												<Popover
 												  open={this.state.menu.open}
 												  anchorEl={this.state.menu.anchorEl}
 												  onRequestClose={this.handleRequestClose}
+												  style={{fontSize: '13px'}}
 												>
 												  <Menu>
-													<MenuItem primaryText="Refresh" />
-													<MenuItem primaryText="Help &amp; feedback" />
-													<MenuItem primaryText="Settings" />
-													<MenuItem primaryText="Sign out" />
+													<MenuItem primaryText="New" style={{fontSize: '13px'}}/>
+													<Divider />
+													<MenuItem primaryText="Save" style={{fontSize: '13px'}}/>
+													<MenuItem primaryText="Save As" style={{fontSize: '13px'}}/>
 												  </Menu>
 												</Popover>
 											</div>
-										 </div>
 										 </Flex>
 								</Flex>
 								
 							  
 							  {/* Row 2 */}
-							  <Flex layout="row" style={{'marginBottom': '0px',height:'50%'}}>
+							  <Flex layout="row" style={{height:'50%'}}>
+							  
 							  </Flex>
 							  
 							  {/* Row 3 */}
-							  <Flex layout="row" style={{'marginBottom': '0px',height:'10%'}}>
+							  <Flex layout="row" style={{height:'15%'}}>
 							  </Flex>
 							  
 							  {/* Row 4 */}
-							  <Flex layout="row" style={{'marginBottom': '0px',height:'25%'}}>
+							  <Flex layout="row" style={{height:'15%'}}>
 							  </Flex>
 				</Flex>				  
 							  {/* BOTTOM SECTION */}
