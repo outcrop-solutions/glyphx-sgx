e102a6c4594ab4bcb04066742b5b0fad4faf8e1d
Bryan-SynGlyphX <bryan@synglyphx.com>
2022-06-03 15:06:18 -0400

    Updated Copy Qt script

---
+++ 3rdparty/scripts/copyQt - Copy.bat
@@ -0,0 +1,20 @@
+
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Core.dll ..\..\bin\Win64\Release\Qt5Core.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Gui.dll ..\..\bin\Win64\Release\Qt5Gui.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Network.dll ..\..\bin\Win64\Release\Qt5Network.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5OpenGL.dll ..\..\bin\Win64\Release\Qt5OpenGL.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Positioning.dll ..\..\bin\Win64\Release\Qt5Positioning.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5PrintSupport.dll ..\..\bin\Win64\Release\Qt5PrintSupport.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Qml.dll ..\..\bin\Win64\Release\Qt5Qml.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5QmlModels.dll ..\..\bin\Win64\Release\QmlModels.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Quick.dll ..\..\bin\Win64\Release\Qt5Quick.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5QuickWidgets.dll ..\..\bin\Win64\Release\Qt5QuickWidgets.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Sql.dll ..\..\bin\Win64\Release\Qt5Sql.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5WebChannel.dll ..\..\bin\Win64\Release\Qt5WebChannel.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5WebEngine.dll ..\..\bin\Win64\Release\Qt5WebEngine.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5WebEngineCore.dll ..\..\bin\Win64\Release\Qt5WebEngineCore.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5WebEngineWidgets.dll ..\..\bin\Win64\Release\Qt5WebEngineWidgets.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5WebSockets.dll ..\..\bin\Win64\Release\Qt5WebSockets.dll
+echo f | xcopy /f /y %QTDIR%\bin\Qt5Widgets.dll ..\..\bin\Win64\Release\Qt5Widgets.dll
+
+pause
