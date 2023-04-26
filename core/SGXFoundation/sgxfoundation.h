f283dfcb86671ee5489a8bf3c9537c93ecd68719
JP Burford <jp.burford@h1insights.com>
2023-04-26 14:44:40 -0400

    Updating ANTzBridge and SgxFoundation for Linux build

--- core/SGXFoundation/sgxfoundation.h
+++ core/SGXFoundation/sgxfoundation.h
@@ -21,8 +21,8 @@
 // that uses this DLL. This way any other project whose source files include this file see 
 // SGXFOUNDATION_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
-
 #include "SGxConfig.h"
+
 #ifdef WIN32
 
 #ifdef SGXFoundation_EXPORTS
