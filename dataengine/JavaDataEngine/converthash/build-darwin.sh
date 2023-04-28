2b8bab92c312b7c276f27794486093d5e10054ce
JP Burford <jp.burford@h1insights.com>
2023-04-28 13:51:42 -0400

    removed more unused projects

--- dataengine/JavaDataEngine/converthash/build-darwin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo Building converthash library...
-c++ -dynamiclib -o libconverthash.dylib -I../../jdk1.7.0_79/include/ -I../../jdk1.7.0_79/include/darwin/ -I../../../3rdParty/include/boost_1_60_0/ HashInput/main.cpp
